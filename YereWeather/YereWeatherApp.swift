bb79f16177677ddb358dc9cc864a15bb95d80070
Diana Amirzadyan <amirzadiana@gmail.com>
2025-05-19 19:59:45 +0400

    Initial Commit

---
+++ YereWeather/YereWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  YereWeatherApp.swift
+//  YereWeather
+//
+//  Created by Dianella on 19.05.25.
+//
+
+import SwiftUI
+
+@main
+struct YereWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
